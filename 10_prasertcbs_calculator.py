5a940b1070d157f7821ec7f9a401dfcc4a8799a3
lunar11120 <lunar11120@gmail.com>
2017-08-12 17:44:16 +0700

    Add files via upload

---
+++ 10_prasertcbs_calculator.py
@@ -0,0 +1,17 @@
+# a = 7
+# b = 3
+# print ("Test calculator")
+# f = 34 + (2**3)
+# print (f)
+def rectangle(w, h):
+    area = w * h
+    return area
+
+def triangle(w , h):
+    return .5 * w * h
+
+w = int(input("Weight = "))
+h = int(input("Height = "))
+
+print ("Area = ",rectangle(w, h))
+print ("Triangle = ",triangle(w, h))
